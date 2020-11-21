daed6c808ba8e4a022f8167d32defc82bc6f8f78
Tiago Valentim Barreto <tiagovalentim@gmail.com>
2020-11-20 23:59:41 -0300

    refactor: improve readability of input fields validation

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -2,16 +2,14 @@ import { HttpRequest, HttpResponse } from '../ports/http'
 
 export class SignupController {
   handle (httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body.name) {
-      return {
-        statusCode: 400,
-        body: new Error('Missing param: name')
-      }
-    }
-    if (!httpRequest.body.email) {
-      return {
-        statusCode: 400,
-        body: new Error('Missing param: email')
+    const fields = ['name', 'email', 'password', 'passwordConfirmation']
+
+    for (const field of fields) {
+      if (!httpRequest.body[field]) {
+        return {
+          statusCode: 400,
+          body: new Error(`Missing param: ${field}`)
+        }
       }
     }
   }
