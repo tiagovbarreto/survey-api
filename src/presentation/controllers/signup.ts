7c222ec5eba658507933ac7de41af76e4158de1b
Tiago Valentim Barreto <tiagovalentim@gmail.com>
2020-11-21 00:46:23 -0300

    refactor: add interface for controllers

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,8 +1,9 @@
 import { MissingParamError } from '../errors/missing-param-error'
 import { badRequest } from '../helpers/http-helper'
+import { Controller } from '../ports/controller'
 import { HttpRequest, HttpResponse } from '../ports/http'
 
-export class SignupController {
+export class SignupController implements Controller {
   handle (httpRequest: HttpRequest): HttpResponse {
     const fields = ['name', 'email', 'password', 'passwordConfirmation']
 
