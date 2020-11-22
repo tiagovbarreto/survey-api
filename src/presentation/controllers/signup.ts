0117b3826a2fd70c9f5f87a5d62797160d82147f
Tiago Valentim Barreto <tiagovalentim@gmail.com>
2020-11-21 23:47:54 -0300

    feat: ensure SignUpController returns 400 if password and password confirmation do not match

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -21,10 +21,16 @@ export class SignUpController implements Controller {
       }
     }
 
-    const isValid = this.emailValidator.isValid(httpRequest.body.email)
+    const { email, password, passwordConfirmation } = httpRequest.body
+
+    const isValid = this.emailValidator.isValid(email)
 
     if (!isValid) {
       return badRequest(new InvalidParamError('email'))
+    }
+
+    if (password !== passwordConfirmation) {
+      return badRequest(new InvalidParamError('passwordConfirmation'))
     }
   }
 }
