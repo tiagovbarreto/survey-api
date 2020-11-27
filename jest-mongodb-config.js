e206d0aab6e9887632d3e896ef51abcbaea47ab9
Tiago Valentim Barreto <tiagovalentim@gmail.com>
2020-11-26 21:50:14 -0300

    chore: add jest configs to mongodb

---
+++ jest-mongodb-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  mongodbMemoryServerOptions: {
+    binary: {
+      version: '3.6.3',
+      skipMD5: true
+    },
+    autoStart: false,
+    instance: {}
+  }
+}
--- jest.config.js
+++ jest.config.js
@@ -10,5 +10,6 @@ module.exports = {
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
   testEnvironment: 'node',
+  preset: '@shelf/jest-mongodb',
   transform: { '.+\\.ts$': 'ts-jest' }
 }
