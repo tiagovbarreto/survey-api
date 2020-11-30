70610ba4a5a856305ece3b749996282ddccbff5d
Tiago Valentim Barreto <tiagovalentim@gmail.com>
2020-11-30 00:27:15 -0300

    refactor: move sut to a factory helper function

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -5,21 +5,38 @@ import {
 } from '../../presentation/protocols'
 import { LogControllerDecorator } from './log'
 
+interface SutTypes {
+  sut: LogControllerDecorator;
+  controllerStub: Controller;
+}
+
+const makeController = () => {
+  class ControllerStub implements Controller {
+    async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+      const httpResponse: HttpResponse = {
+        statusCode: 200,
+        body: {}
+      }
+      return httpResponse
+    }
+  }
+
+  return new ControllerStub()
+}
+
+const makeSut = (): SutTypes => {
+  const controllerStub: Controller = makeController()
+  const sut = new LogControllerDecorator(controllerStub)
+
+  return {
+    sut,
+    controllerStub
+  }
+}
+
 describe('Log Controller  Decorator', () => {
   test('should call controller handle', async () => {
-    class ControllerStub implements Controller {
-      async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-        const httpResponse: HttpResponse = {
-          statusCode: 200,
-          body: {}
-        }
-        return httpResponse
-      }
-    }
-
-    const controllerStub = new ControllerStub()
-
-    const sut = new LogControllerDecorator(controllerStub)
+    const { sut, controllerStub } = makeSut()
 
     const httpRequest: HttpRequest = {
       body: {
